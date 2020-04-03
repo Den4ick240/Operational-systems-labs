12f221eb74445c861c2ac77b0a9584eaaae2c647
Den4ick240 <45475878+Den4ick240@users.noreply.github.com>
2020-04-03 14:02:10 +0700

    Add files via upload

---
+++ lab4.c
@@ -0,0 +1,69 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "errno.h"
+
+#define MAX_STRING_LENGTH 4096
+
+
+struct StringListNode {
+    char *str;
+    struct StringListNode *next;
+};
+
+
+int main() {
+    struct StringListNode *head = NULL,
+            *buffNode = NULL;
+    char *inputBuffer = NULL;
+    int outputCode = 0;
+
+	printf("Write some strings, and then write \".\" to end the programm\n");
+
+    inputBuffer = (char *) malloc(sizeof(char) * MAX_STRING_LENGTH);
+    if (inputBuffer == NULL) {
+        perror(strerror(errno));
+        exit(0);
+    }
+
+    while (1) {
+        if (NULL == fgets(inputBuffer, MAX_STRING_LENGTH, stdin)) {
+            outputCode = 1;
+            perror(strerror(errno));
+            break;
+        }
+
+        if (inputBuffer[0] == '.') {
+            break;
+        }
+
+        buffNode = (struct StringListNode *) malloc(sizeof(struct StringListNode));
+        if (buffNode == NULL) {
+            outputCode = 1;
+            perror(strerror(errno));
+            break;
+        }
+
+        buffNode->str = (char *) malloc(sizeof(char) * (strlen(inputBuffer) + 1));
+        if (buffNode->str == NULL) {
+            outputCode = 1;
+            free(buffNode);
+            perror(strerror(errno));
+            break;
+        }
+        strcpy(buffNode->str, inputBuffer);
+        buffNode->next = head;
+        head = buffNode;
+    }
+
+    free(inputBuffer);
+    while (head != NULL) {
+        printf("%s", head->str);
+        buffNode = head;
+        head = head->next;
+        free(buffNode->str);
+        free(buffNode);
+    }
+
+    exit(outputCode);
+}
