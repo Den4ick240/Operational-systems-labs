1ed237f97a7366156676a3c2833d2b5d93e6900b
Den4ick240 <45475878+Den4ick240@users.noreply.github.com>
2020-04-10 13:18:42 +0700

    Add files via upload

--- 4/lab4.c
+++ 4/lab4.c
@@ -10,19 +10,15 @@ struct StringListNode {
     struct StringListNode *next;
 };
 
+void printAndDestroyList(struct StringListNode *head);
+
 int main() {
     struct StringListNode *head = NULL,
             *buffNode = NULL;
-    char *inputBuffer = NULL;
-    int outputCode = 0;
+    char inputBuffer[MAX_STRING_LENGTH];
+	int outputCode = 0;
 
     printf("Enter some lines. Enter \".\" and programm will print all the lines you entered\n");
-
-    inputBuffer = (char *) malloc(sizeof(char) * MAX_STRING_LENGTH);
-    if (inputBuffer == NULL) {
-        perror(strerror(errno));
-        exit(0);
-    }
 
     while (1) {
         if (NULL == fgets(inputBuffer, MAX_STRING_LENGTH, stdin)) {
@@ -42,19 +38,17 @@ int main() {
             break;
         }
 
-        buffNode->str = (char *) malloc(sizeof(char) * (strlen(inputBuffer) + 1));
-        if (buffNode->str == NULL) {
-            outputCode = 1;
-            free(buffNode);
-            perror(strerror(errno));
-            break;
-        }
-        strcpy(buffNode->str, inputBuffer);
+        buffNode->str = strdup(inputBuffer);
         buffNode->next = head;
         head = buffNode;
     }
 
-    free(inputBuffer);
+	printAndDestroyList(head);
+    exit(outputCode);
+}
+
+void printAndDestroyList(struct StringListNode *head) {
+	struct StringListNode *buffNode;
     while (head != NULL) {
         printf("%s", head->str);
         buffNode = head;
@@ -62,6 +56,5 @@ int main() {
         free(buffNode->str);
         free(buffNode);
     }
-
-    exit(outputCode);
 }
+
