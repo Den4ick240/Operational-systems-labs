78c08df7c6a01614da8d0aefe783b7bac42618fe
Den4ick240 <45475878+Den4ick240@users.noreply.github.com>
2020-04-24 12:21:30 +0700

    Add files via upload

--- 5/lab5.c
+++ 5/lab5.c
@@ -4,90 +4,120 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#define SIZE 1024
+#define READING_BUFFER_SIZE 8
+#define MAX_NUMBER_OF_LINES 1024
 
-int buildFileStringTable(int *endls, int fd) {
-	int n = 1, i = 1;
-	char buff;
-	endls[0] = 0;
-	while (read(fd, &buff, sizeof(char) > 0 && n < SIZE)) {
-		if (buff == '\n') {
-			endls[n++] = i;
+int buildLinesTable(int fileDescriptor, int *lineOffsets);
+void printLine(int fileDescriptor, int *lineOffsets, int lineNumber);
+int readNumber(int numberOfLines);
+
+int main (int argc, char **argv) {
+	int fileDescriptor;
+	int lineOffsets[MAX_NUMBER_OF_LINES + 1];
+	int numberOfLines, i = 0;
+	
+	if (argc < 2) {
+		printf("usage: %s file_name\n", argv[0]);
+		exit(0);
+	}
+	
+	fileDescriptor = open(argv[1], O_RDONLY);
+	if (fileDescriptor == -1) {
+		printf("Couldn't open file");
+		perror(strerror(errno));
+		exit(1);
+	}
+	
+	numberOfLines = buildLinesTable(fileDescriptor, lineOffsets);
+
+	while ((i = readNumber(numberOfLines)) != 0) {
+		if (i == -1) {
+			continue;
 		}
-		i++;
+		printf("line number %d:\n", i);
+		printLine(fileDescriptor, lineOffsets, i);
 	}
-	if (i - 1 != endls[n - 1]) {
-		endls[n++] = i;
-	}
-	printf("File contains %d %s\n", n - 1, (n == 2 ? "line" : "lines"));
-	return n;
 }
 
-void printString(int *endls, int id, int fd) {
-	char *strBuff = NULL;
-	int size = endls[id] - endls[id - 1];
+int buildLinesTable(int fileDescriptor, int *lineOffsets) {
+	char readingBuffer[READING_BUFFER_SIZE];
+	int bufferCounter, readedBytesNum, i, numberOfLines;
 	
-	strBuff =  (char *)malloc(sizeof(char) * (size));
-	if (strBuff == NULL) {
+	numberOfLines = 0;
+	bufferCounter = 0; 
+	readedBytesNum = 0;
+	i = 0;
+	
+	lineOffsets[0] = -1;
+	
+	while (1) {
+		if (bufferCounter == readedBytesNum) {
+			bufferCounter = 0;
+			readedBytesNum = read(fileDescriptor, &readingBuffer, sizeof(char) * READING_BUFFER_SIZE);
+		}
+		if (readedBytesNum == 0 && errno == EINTR) {
+			continue;
+		}
+		if (readedBytesNum == 0 || numberOfLines >= MAX_NUMBER_OF_LINES) {
+			break;
+		}
+		if (readingBuffer[bufferCounter] == '\n') {
+			numberOfLines++; 
+			lineOffsets[numberOfLines] = i;
+		}				
+		i++;
+		bufferCounter++;
+	}
+	if (lineOffsets[numberOfLines] != i - 1 && numberOfLines < MAX_NUMBER_OF_LINES) {
+		numberOfLines++; 
+		lineOffsets[numberOfLines] = i;
+	}
+	printf("File contains %d line%s\n", numberOfLines, numberOfLines == 1 ? "" : "s");
+	return numberOfLines;
+}
+
+void printLine(int fileDescriptor, int *lineOffsets, int lineNumber) {
+	char *line = NULL;
+	int lineSize;
+	int readedBytesNumber;
+	
+	lineSize = 	lineOffsets[lineNumber] - lineOffsets[lineNumber - 1];
+	
+	line = (char *)malloc(sizeof(char) * lineSize);
+	if (line == NULL) {
+		printf("Error: couldn't allocate memory of size %d\n", lineSize);
 		perror(strerror(errno));
 		return;
 	}
-	lseek(fd, endls[id - 1], SEEK_SET);
-	read(fd, strBuff, sizeof(char) * (size - 1));
-	strBuff[size - 1] = 0;
-	printf("%s\n", strBuff);
-	free(strBuff);
-		
+	
+	lseek(fileDescriptor, lineOffsets[lineNumber - 1] + 1, SEEK_SET);
+	do {
+		readedBytesNumber = read(fileDescriptor, line, sizeof(char) * (lineSize - 1));
+	} while (readedBytesNumber == -1 && errno == EINTR);
+	
+	line[lineSize - 1] = 0;
+	printf("%s\n", line);
+	free(line);
 }
 
-int scanNumber(int n) {
-	int id, scanfResult = 0;
-	scanfResult = scanf("%d", &id);
-
-	if (scanfResult == 0 || id <= 0) {
-		printf("Enter a number from 1 to %d\n", n - 1);
+int readNumber(int numberOfLines) {
+	int scanfResult;
+	int enteredNumber;
+	
+	do {
+		scanfResult = scanf("%d", &enteredNumber);
+	} while (scanfResult == EOF && errno == EINTR);
+	
+	if (scanfResult == EOF) {
+		printf("Error: couldn't read from input stream, try again\n");
+		perror(strerror(errno));
+		return -1;
+	}
+	
+	if (scanfResult == 0 || enteredNumber < 0 || numberOfLines < enteredNumber) {
+		printf("Bad input, enter a number from 1 to %d, or enter 0 to exit\n", numberOfLines);
 		while (scanfResult == 0 && getc(stdin) != '\n');
-		return 0;
+		return -1;
 	}
-	if (id >= n) {
-		printf("Entered number is too big\n");
-		return 0;
-	}
-	return id;
-}
-
-int main(int argc, char **argv) {
-	int fd;
-	int endls[SIZE];
-	int n, i;
-	
-	if (argc < 2) {
-		printf("Enter a file name as an argument\n");
-		exit(0);
-	}
-
-	if ((fd = open(argv[1], O_RDONLY)) == -1) {
-		perror(strerror(errno));
-		exit(1);
-	}
-	
-	n = buildFileStringTable(endls, fd);
-	
-	if (n == 0) {
-		return 0;
-	}
-	
-	while (1) {
-		int id = scanNumber(n);
-		if (id == 0) {
-			continue;
-		}
-		printString(endls, id, fd);
-	}
-	
-	if (close(fd) == -1) {
-		perror(strerror(errno));
-		exit(1);
-	}
-	return 0;
+	return enteredNumber;
 }
