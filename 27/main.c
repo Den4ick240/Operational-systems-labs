48911c0740987b5e68389d2284038d2273d20f8c
Den4ick240 <45475878+Den4ick240@users.noreply.github.com>
2020-05-29 14:34:46 +0700

    Add files via upload

--- 27/main.c
+++ 27/main.c
@@ -4,34 +4,18 @@
 #define BUFF_SIZE 8
 
 int main(int argc, char **argv) {
-	FILE *fin = NULL, *pout = NULL;
+	FILE *pin = NULL;
 	char buff[BUFF_SIZE];
-	if (argc < 2) {
-		printf("usage: %s fileName\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	fin = fopen(argv[1], "r");
-	if (fin == NULL) {
-		printf("Couldn't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	pout = popen("wc -l", "w");
-	if (pout == NULL) {
+	sprintf(buff, "cat %s | grep '^$' | wc -l", argv[1]);
+	pin = popen(buff, "r");
+	if (pin == NULL) {
 		printf("Couldn't open pipe\n");
-		if (fclose(fin) == -1) {
-			printf("Couldn't close file %s\n", argv[1]);
-		}
 		exit(EXIT_FAILURE);
 	}
-	while (fgets(buff, BUFF_SIZE, fin) != NULL) {
-		if (*buff == '\n') {
-			fputs(buff, pout);
-		}
-	}
-	if (fclose(fin) == -1) {
-		printf("Couldn't close file %s\n", argv[1]);
-	}
-	if (pclose(pout) == -1) {
+	int a;
+	fscanf(pin, "%d", &a);
+	printf("%d\n", a);
+	if (pclose(pin) == -1) {
 		printf("Couldn't close pipe\n");
 	}
 	exit(EXIT_SUCCESS);
